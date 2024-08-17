cc6f2debe56e53b6ebce25759e9b0a09b55eda7f
binuka2009 <binukaudayanga3@gmail.com>
2024-08-17 12:35:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/982640de2d7f18fced629.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I Am Mr_Binu_MD I Am Alive Now...✔",
 };
