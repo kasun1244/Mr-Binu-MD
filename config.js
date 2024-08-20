a53abfed04e0d997fdee23597d5858cf70edce16
binuka2009 <binukaudayanga3@gmail.com>
2024-08-20 10:20:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/982640de2d7f18fced629.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "👋 *ʜᴇʟʟᴏ ᴅᴇᴀᴇ, ɪ'ᴍ ʙɪɴᴜ ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. . . 👋*
-
- ɪ'ᴍ   ᴀ ʟ ɪ ᴠ ᴇ   ɴ ᴏ ᴡ . . . .🍃
-
-> `Platform - Render`
-> `Created By - Binuka`
-> `Prefix - "."`
-
-👉 ɢʀᴏᴜᴘ - https://chat.whatsapp.com/BanbIuf6wtI14pCbtWy2i6
-
-👉 ᴄʜᴀɴɴᴇʟ - https://whatsapp.com/channel/0029VagVrb63rZZcQ9HRX32y
-
-👉 ᴏᴡɴᴇʀ  - +94769089430
-                      +94724659430
-
-*🌹☺️ - ʜᴀᴠᴇ ᴀ ɴɪᴄᴇ ᴅᴀʏ - ☺️🌹*",
+ALIVE_MSG: process.env.ALIVE_MSG || "👋 *ʜᴇʟʟᴏ ᴅᴇᴀᴇ, ɪ'ᴍ ʙɪɴᴜ ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. . . 👋*",
 };
