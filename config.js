eb27e569c219b70d997af5d86f501b794950c384
binuka2009 <binukaudayanga3@gmail.com>
2024-08-20 10:36:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/982640de2d7f18fced629.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "👋 *ʜᴇʟʟᴏ ᴅᴇᴀᴇ, ɪ'ᴍ ʙɪɴᴜ ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. . . 👋*",
+ALIVE_MSG: process.env.ALIVE_MSG || "👋 ʜᴇʟʟᴏ ᴅᴇᴀʀ, ɪ'ᴍ ʙɪɴᴜ ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ. . . 👋",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
