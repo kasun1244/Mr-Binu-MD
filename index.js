59d9f639816de3efe85df7dcbba6e51232c934b8
binuka2009 <binukaudayanga3@gmail.com>
2024-08-20 10:49:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-let up = `*BINU MD CONNECTED SUCCESSFUL...✅\n\nPREFIX: ${prefix}`;
+let up = `*BINU MD CONNECTED SUCCESSFUL...✅*\n\nPREFIX: ${prefix}`;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/982640de2d7f18fced629.jpg` }, caption: up })
 
