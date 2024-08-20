d65286e574f9303ad5e4eb518e2d1810d7c45b5c
binuka2009 <binukaudayanga3@gmail.com>
2024-08-20 23:50:23 +0530

    Update Fb.js

--- plugins/Fb.js
+++ plugins/Fb.js
@@ -1,6 +1,5 @@
 const {cmd , commands} = require('../command')
 const fbVideoDownloader = require('fb-video-downloader')
-const videoUrl = 'https://www.facebook.com/your-video-url';
 
 cmd({
     pattern: "fb",
@@ -19,25 +18,7 @@ if(!q) return reply("Please Give Me Your Fb URL...⚡")
     }
     console.log('Video Data:', data);
     // data will contain the video URL and other details
-};
-
-let desc = `
-𝗕𝗜𝗡𝗨 𝗠𝗗 𝗕𝗢𝗧 𝗙𝗕 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥...🌟
-
-*Title* ► ${data.title}
-
-*Description* ► ${data.description}
-
-*Time* ► ${data.duration}
-
-*Ago* ► ${data.ago}
-
-*Views* ► ${data.views}
-
-
-𝑃𝑂𝑊𝐸𝑅𝐸𝐷 𝐵𝑌 𝐵𝐼𝑁𝑈𝐾𝐴
-`
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});    
+};    
 
 //========== Download Audio ==========
 
@@ -45,8 +26,7 @@ let down = await fbVideoDownloader(url)
 let downloadUrl = down.dl_url
 
 //========== Send Audio And Audio Document Message ==========
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4"},{quoted:mek})
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
     
 }catch(e){
 console.log(e)
