e518935941c048d8f009681f85f9caa5f2f45f89
binuka2009 <binukaudayanga3@gmail.com>
2024-08-21 18:42:01 +0530

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,5 +1,11 @@
+const {cmd , commands} = require('../command')
 import fg from 'api-dylux';
-
+cmd({
+    pattern: "facebook",
+    desc: "download facebokk",
+    category: "download",
+    filename: __filename
+},
 const handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
     throw `✳️ කරුණාකර ෆෙස්බුක් වෙබ් ලිපිනය යොදන්න / Please send the link of a Facebook video\n\n📌 EXAMPLE :\n*${usedPrefix + command}* https://www.facebook.com/Ankursajiyaan/videos/981948876160874/?mibextid=rS40aB7S9Ucbxw6v`;
@@ -8,9 +14,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
   if (!urlRegex.test(args[0])) {
     throw '⚠️ කරුණාකර වලංගු වෙබ් ලිපිනයක් යොදන්න /PLEASE GIVE A VALID URL.';
-  }
-
-  m.react(rwait);
+  };
 
   try {
     const result = await fg.fbdl(args[0]);
@@ -24,16 +28,9 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     const videoBuffer = Buffer.from(arrayBuffer);
 
     conn.sendFile(m.chat, videoBuffer, 'fb.mp4', tex, m);
-    m.react(done);
   } catch (error) {
     console.log(error);
     m.reply('⚠️කරුණාකර නැවත උත්සහ කරන්න / An error occurred while processing the request. Please try again later.');
   }
 };
 
-handler.help = ['facebook <url>'];
-handler.tags = ['dl'];
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i;
-handler.diamond = true;
-
-export default handler;
