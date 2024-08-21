43f518b02264a08b2cb496e977be43bc7d29f75f
binuka2009 <binukaudayanga3@gmail.com>
2024-08-21 16:59:36 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,7 +1,6 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require(os)
-
 cmd({
     pattern: "system",
     alias: ["panel","infobot","status"],
@@ -11,11 +10,12 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `> Uptime: ${runtime(process.uptime())}
-> Ram Usage: ${(process.memoryUsage().helpUsed / 1024 / 1024).toFixed(2)}MB /${Math.round(require('os').totalmem / 1024 /1024)}MB
-> Host Platform: ${(os.hostname)}
-> Owner: Binuka Lasad Udayanga
+let status = `> Uptime ► ${runtime(process.uptime())}
+> Ram Usage ► ${(process.memoryUsage().helpUsed / 1024 / 1024).toFixed(2)}MB /${Math.round(require('os').totalmem / 1024 /1024)}MB
+> Host Platform ► ${(os.hostname)}
+> Owner ► Binuka Lasad Udayanga
 `
+return reply(`${status}`)
 }catch(e){
 console.log(e)
 reply(`${e}`)
