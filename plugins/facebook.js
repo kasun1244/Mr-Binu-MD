800a91a89e90f7c6383267b57c554705b80444d6
binuka2009 <binukaudayanga3@gmail.com>
2024-08-21 19:46:29 +0530

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,28 +1,42 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 
-//=========== Video DL ============
-
 cmd({
     pattern: "fb",
-    desc: "download fb",
+    desc: "downloa songs",
     category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please Give Me Your Video Name...⚡")
-const search = await ymp4(q)
+if(!q) return reply("Please Give Me Your Song Name...⚡")
+const search = await fg.fbdl(q)
 const data = search.videos[0];
 const url = data.url
-    
-//========= Download Video ==========
 
-let down = await fg.ytmp4(url)
+let desc = `
+𝗕𝗜𝗡𝗨 𝗠𝗗 𝗕𝗢𝗧 𝗦𝗢𝗡𝗚 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥...🌟
+
+*Title* ► ${data.title}
+
+*Description* ► ${data.description}
+
+*Time* ► ${data.duration}
+
+*Ago* ► ${data.ago}
+
+*Views* ► ${data.views}
+
+ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙɪɴᴜ ᴍᴅ`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});    
+
+//========== Download Audio ==========
+
+let down = await fg.fbdl(url)
 let downloadUrl = down.dl_url
 
-//========== Send Video And Video Document Message ==========
-await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+//========== Send Audio And Audio Document Message ==========
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
     
 }catch(e){
 console.log(e)
