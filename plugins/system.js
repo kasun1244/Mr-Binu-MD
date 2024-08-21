be901a49e1eb47f7aa4dbea415dbbf4f07ffc20f
binuka2009 <binukaudayanga3@gmail.com>
2024-08-21 16:34:03 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,11 +1,17 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const os = require(os)
 
 cmd({
-    pattern: "alive",
+    pattern: "system",
+    alias: ["panel","infobot","status"],
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
