dbfcc9bfc9fa808778220ca194e0ecc18200ac64
binuka2009 <binukaudayanga3@gmail.com>
2024-08-21 17:54:20 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,18 +12,27 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let status = `- 𝗦𝗬𝗦𝗧𝗘𝗠 𝗦𝗧𝗔𝗧𝗨𝗦 -
+let status = `𝗪𝗘𝗟𝗖𝗢𝗠𝗘 𝗧𝗢 𝗦𝗬𝗦𝗧𝗘𝗠 𝗦𝗧𝗔𝗧𝗨𝗦..🧬
 
 > Uptime ► ${runtime(process.uptime())}
 
-> Ram Usage ► ${(process.memoryUsage().helpUsed / 1024 / 1024).toFixed(2)}MB /${Math.round(require('os').totalmem / 1024 /1024)}MB
+> Ram Usage ► ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB /${Math.round(require('os').totalmem / 1024 /1024)}MB
 
 > Host Platform ► ${(os.hostname)}
 
+> Prefix ► "."
+
 > Owner ► Binuka Lasad Udayanga
+
+> Owner Contact ► +94769089430
 
 
 *_Group_* ► https://chat.whatsapp.com/BanbIuf6wtI14pCbtWy2i6
+
+
+*_Channel_* ► https://whatsapp.com/channel/0029VagVrb63rZZcQ9HRX32y
+
+©️ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙɪɴᴜ ᴍᴅ
 `
 return reply(`${status}`)
 }catch(e){
